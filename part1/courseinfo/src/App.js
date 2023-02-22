8fb599284aabe7d93491fff89f78d9c1c5f8fa5b
jurada <tokontoszkolne@gmail.com>
2023-02-22 17:15:12 +0100

    make Content component render a div

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,11 +8,11 @@ const Part = (props)=>(
 )
 
 const Content = (props)=>(
-  <>
+  <div>
     <Part part={props.part1} exercises={props.exercises1}/>
     <Part part={props.part2} exercises={props.exercises2}/>
     <Part part={props.part3} exercises={props.exercises3}/>
-  </>
+  </div>
 )
 
 
