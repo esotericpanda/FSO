e7ccd86c96ab26c39f7507ef6e2d47b5ea412bed
jurada <tokontoszkolne@gmail.com>
2023-02-24 21:23:58 +0100

    finish exercise 2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,6 +12,9 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad
+  const average= (good-bad)/(total)
+  const positivePercent=(100*good)/(total)
   return (
 
     <div>
@@ -24,6 +27,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercent}%</p>
     </div>
   )
 }
