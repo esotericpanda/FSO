2d313b48b757b3ddda04b888c3a02ee1be0e2e60
jurada <tokontoszkolne@gmail.com>
2023-02-25 15:29:18 +0100

    Finish exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,29 +1,4 @@
-const Header = ({ name }) => <h1>{name}</h1>
-
-const Total = ({ parts }) => <p style={{fontWeight: 'bold'}}>Number of exercises {parts.reduce((a,b)=>a+b.exercises, 0)}</p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part=><Part part={part} key={part.id} />)}    
-  </>
-
-
-const Course = ({course})=>{
-  
-  return(
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts} />
-    </div>
-  )
-
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
